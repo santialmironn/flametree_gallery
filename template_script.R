fd83385133e82306a8734c28f6b6c922527cff2d
Vicky Marull <vickymarull@Air-de-Vicky.fibertel.com.ar>
2024-09-20 11:23:08 -0300

    Arbol de vicky

--- template_script.R
+++ template_script.R
@@ -1,13 +1,13 @@
 library(flametree)
 
 # your name, without spaces or special characters
-name <- "fern"
+name <- "Victoria"
 
 # pick a seed
 this_seed <- 422576537
 
 # pick some colours
-shades <- c("red", "green", "blue", "orange")
+shades <- c("darkseagreen3", "#E0FFFF", "#FFE4E1", "#FF69B4")
 
 # data structure defining the trees
 dat <- flametree_grow(seed = this_seed, time = 10, trees = 10)
@@ -23,3 +23,4 @@ tree <- dat %>%
 tree
 # save the plot
 flametree_save(tree, filename = paste0("fig/tree_", name, ".png"))
+
