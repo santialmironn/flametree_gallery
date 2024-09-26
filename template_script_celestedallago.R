7eee6634476f118b951d555a9d91cecc00115990
celestedallago <celedallago@gmail.com>
2024-09-26 19:53:32 -0300

    Arbol de celeste

---
+++ template_script_celestedallago.R
@@ -0,0 +1,27 @@
+# install.packages('flametree')
+library(flametree)
+
+# your name, without spaces or special character
+name <- "Celeste"
+
+# pick a seed
+this_seed <- 234566537
+
+# pick some colours
+shades <- c("pink", "purple", "blue", "skyblue")
+
+# data structure defining the trees
+dat <- flametree_grow(seed = this_seed, time = 10, trees = 10)
+
+# draw the plot
+tree <- dat %>%
+  flametree_plot(
+    background = "antiquewhite",
+    palette = shades,
+    style = "nativeflora"
+  )
+
+tree
+# save the plot
+flametree_save(tree, filename = paste0("fig/tree_", name, ".png"))
+
